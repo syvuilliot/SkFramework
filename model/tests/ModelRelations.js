a21fe09c41f7a434ef29a69f164c75e1a606bb4d
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-05-07 11:08:26 +0200

    new tests for Model

--- model/tests/ModelRelations.js
+++ model/tests/ModelRelations.js
@@ -8,15 +8,14 @@
 	//************* Models
 	Model.store = new Memory();
 	
-	var Person = declare("Sk.Person", [Model], {
+	window.Person = declare("Sk.Person", [Model], {
 		getage: function(){
 			return 2012 - this.get("birthYear");
 		},
 		getchildren: function(){
-			var self = this;
-			return instancesRegistry.query(function(item){
-				return item.isInstanceOf(Person) && (item.father.indexOf(self.getIdentity()) >= 0 || item.mother.indexOf(self.getIdentity()) >= 0);
-			});
+			var union = this.get("motherOf");
+			union.push(this.get("fatherOf"));
+			return union;
 		},
 	});
 	
@@ -67,56 +66,97 @@
 		max: 1,
 	});
 	
-//*********** Tests
+//*********** Instances
 	window.syv = new Person({
 		id: "1",
 		name: "Syv",
 		birthYear: 1982,
 	});
+	syv.save();
 
-	doh.register("Simple getter test", function(t){
-		t.is(syv.get("age"), 30, "syv should be 30");
-	});
-	/*
 	window.ket = new Person({name: "Ket"});
+	ket.save();
 	window.aur = new Person({name: "Aurélie"});
+	aur.save();
 	window.ant = new Person({name: "Antonin"});
 	ant.add("father", syv);
 	ant.add("mother", aur);
+	ant.save();
 	
 	aur.add("conjoint", syv);
+	aur.save();
 	
 	window.todo1 = new Todo({
 		id: "2",
 		title: "Concevoir la couche Model",
-		//user: syv,
 	});
 	todo1.add("user", syv);
+	todo1.save();
 	
 	window.todo2 = new Todo({id: "3", title: "Concevoir la couche UI"});
 	todo2.add("user", syv);
+	todo2.save();
 	
 	window.todo3 = new Todo({title:"Lancer Maponaute"});
 	todo3.add("user", ket);
+	todo3.save();
 	
 	window.tag1 = new Tag({label: "test"});
+	tag1.save();
 	window.tag2 = new Tag({label: "cool"});
+	tag2.save();
 	window.tag3 = new Tag({label: "top"});
+	tag3.save();
 	
 	todo1.add("tags", tag1);
 	todo1.add("tags", tag2);
+	todo1.save();
 	tag1.add("todos", todo2);
+	todo2.save();
 
-	console.log("L'age de syv est:", syv.get("age"));
-	console.log("Le nom du responsable de la tache 1 est :", todo1.get("user")[0].get("name"));
-	console.log("Les taches de Syv sont:", syv.get("todos"));
-	console.log("Les personnes connues dans l'application sont:", instancesRegistry.query({declaredClass:"Sk.Person"}));//permet de retrouver les instances directes d'une classe
-	console.log("Les instances de Model connues dans l'application sont:", instancesRegistry.query(function(item){return item.isInstanceOf(Model)}));//permet de retrouver les instances d'une classe et de ses sous classes
-	console.log("Les tags de la todo1 sont:", todo1.get("tags"));
-	console.log("Les todos tagguées 'test' sont:", tag1.get("todos"));
+//******* Tests
+	//function to compare that 2 arrays contain the same elements but possibly in a different order
+	function sameArray(a1, a2){
+		if (a1.length != a2.length){return false}
+		var delta = false;
+		a1.forEach(function(el){
+			if(a2.indexOf(el) == -1){delta=true}
+		});
+		return !delta;
+	};
+	
+	doh.register("Simple getter", function test0(t){
+		t.is(30, syv.get("age"), "syv should be 30");
+	});
+	doh.register("Relation resolution", {
+		"user of todo1": function (t){
+			t.is(syv, todo1.get("user")[0], "The user of todo1 should be syv");
+		},
+		"syv's todos": function (t){
+			t.t(sameArray(syv.get("todos"), [todo2, todo1]));
+		},
+		"todo1 tags": function(t){
+			t.t(sameArray(todo1.get("tags"), [tag1, tag2]));
+		},
+		"todos with test tag": function(t){
+			tag1.get("todos")
+		},
+	});
+	doh.register("Models query", {
+		"Person instances": function(t){
+			//query based on declaredClass name
+			t.t(sameArray(Model.store.query({declaredClass:"Sk.Person"}), [syv, aur, ket, ant]));
+		},
+		"all Model instances": function(t){
+			//query based on isInstanceOf
+			t.t(sameArray(Model.store.query(function(item){return item.isInstanceOf(Model)}), [syv, aur, ket, ant, todo1, todo2, todo3, tag1, tag2, tag3]));
+		},
+	});
+	/*
+	console.log("Les tags de la todo1 sont:", );
+	console.log("Les todos tagguées 'test' sont:", );
 	console.log("Le conjoint de syv est:", syv.get("conjoint"));
 	console.log("Le conjoint de Aurélie est:", aur.get("conjoint"));
 	console.log("Les enfants de Aurélie sont:", aur.get("children"));
 	*/
-
 });
