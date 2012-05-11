d277ea919fefae3696cee5ad4dd89c1341623b74
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-05-11 16:32:44 +0200

    removed utils/array

--- store/tests/Constructor.js
+++ store/tests/Constructor.js
@@ -1,11 +1,10 @@
 ﻿define([
 	"doh/runner",
-	"SkFramework/utils/array",
 	"SkFramework/utils/identical",
 	"../Constructor",
 	"../LocalStorage",
 	"dojo/_base/declare",
-], function(doh, arrayUtils, identical, Constructor, LocalStorage, declare){
+], function(doh, identical, Constructor, LocalStorage, declare){
 
 	var Person = declare("Person", [], {
 		constructor: function(params){
@@ -20,6 +19,7 @@
 			return 2012 - this.birthYear;
 		}
 	});
+	
 	var Worker = declare("Worker", [Person], {
 		constructor: function(params){
 			this.job = params.job;
@@ -45,25 +45,24 @@
 	window.titi = new Worker({id:"2", name: "titi", job: "coder", birthYear:1980});
 	var titiId = store.put(titi);
 	
-	var sameArray = arrayUtils.sameArray;
-	
 	doh.i = doh.identical = function(expected, actual, sortArrays, hint){
 		if (! identical(expected, actual, sortArrays)){
 			throw new doh._AssertFailure("assertEqual() failed:\n\texpected\n\t\t"+JSON.stringify(expected)+"\n\tbut got\n\t\t"+JSON.stringify(actual)+"\n\n", hint);
 		}
 		return true;
 	};
-	
-	doh.register("Constructor store test",[
-		function personInstance(t){
+
+
+	doh.register("Constructor store test", {
+		"Person instance": function(t){
 			t.i(toto, store.get(totoId));
 			t.i("my name is toto", store.get(totoId).describe());
 		},
-		function workerInstance(t){
+		"Worker instance": function(t){
 			t.i(titi, store.get(titiId));
 			t.i("my name is titi and my job is coder", store.get(titiId).describe());
 		},
-		function queryAll(t){
+		"query all": function(t){
 			window.allInstances = store.query({});
 			t.i(2, allInstances.total);
 			t.i([toto, titi], allInstances, true);
@@ -72,10 +71,10 @@
 				t.t(item.describe);
 			});
 		},
-		function queryOnInstanceMethod(t){
+		"queryOnInstanceMethod": function(t){
 			window.moreThanThirty = store.query(function(item){return item.getAge()>30});
 			t.i([titi], moreThanThirty);
 		},
-	]);
+	});
 	
 });
