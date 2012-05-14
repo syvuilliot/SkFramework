79f780efd0021203b88da32b422258caf132dbb5
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-05-14 20:18:43 +0200

    Based on dojo/store/util/SimpleQueryEngine but use "get" method on objects if available

---
+++ store/SimpleQueryEngineGet.js
@@ -0,0 +1,71 @@
+define(["dojo/_base/array"], function(arrayUtil) {
+
+return function(query, options){
+	// summary:
+	//		Based on dojo/store/util/SimpleQueryEngine but use "get" method on objects if available
+	//
+
+	// create our matching query function
+	switch(typeof query){
+		default:
+			throw new Error("Can not query with a " + typeof query);
+		case "object": case "undefined":
+			var queryObject = query;
+			query = function(object){
+				for(var key in queryObject){
+					var required = queryObject[key];
+					if(object.get){
+						var propValue = object.get(key);
+					} else {
+						var propValue = object[key];
+					}
+					if(required && required.test){
+						// an object can provide a test method, which makes it work with regex
+						if(!required.test(propValue, object)){
+							return false;
+						}
+					}else if(required != propValue){
+						return false;
+					}
+				}
+				return true;
+			};
+			break;
+		case "string":
+			// named query
+			if(!this[query]){
+				throw new Error("No filter function " + query + " was found in store");
+			}
+			query = this[query];
+			// fall through
+		case "function":
+			// fall through
+	}
+	function execute(array){
+		// execute the whole query, first we filter
+		var results = arrayUtil.filter(array, query);
+		// next we sort
+		if(options && options.sort){
+			results.sort(function(a, b){
+				for(var sort, i=0; sort = options.sort[i]; i++){
+					var aValue = a[sort.attribute];
+					var bValue = b[sort.attribute];
+					if (aValue != bValue) {
+						return !!sort.descending == aValue > bValue ? -1 : 1;
+					}
+				}
+				return 0;
+			});
+		}
+		// now we paginate
+		if(options && (options.start || options.count)){
+			var total = results.length;
+			results = results.slice(options.start || 0, (options.start || 0) + (options.count || Infinity));
+			results.total = total;
+		}
+		return results;
+	}
+	execute.matches = query;
+	return execute;
+};
+});
