ec6659d0ccb3424d5f02e6717a197d338ea33b99
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-05-14 16:50:18 +0200

    use "sk/utils/create" instead of "dojo/_base/declare"

--- model/Model.js
+++ model/Model.js
@@ -1,50 +1,65 @@
 ﻿define([
 	"dojo/_base/lang",
-	"dojo/_base/declare",
-], function(lang, declare){
-	var Model = declare("Sk.Model", [], {
-		constructor: function(params){
+	"SkFramework/utils/create",
+], function(lang, create){
+	var Model = create(null, function Model(params){
 			lang.mixin(this, params);
-		},
-		validate: function(){return true},
-		save: function(){
-			if (this.validate()){Model.store.put(this)}
-		},
-		getIdentity: function(){
-			return Model.store.getIdentity(this);
-		},
-		get: function(propertyName){
-			if (this["get"+propertyName]){
-				//if a getter is defined
-				return this["get"+propertyName]();
-			} else {
-				return this[propertyName];
-			}
-		},
-		set: function(propertyName, value){
-			if (this["set"+propertyName]){
-				//if a setter is defined
-				this["set"+propertyName](value);
-			} else {
-				this[propertyName]=value;
-			}
-		},
-		add: function(propertyName, value){
-			if (this["add"+propertyName]){
-				//if a adder is defined
-				this["add"+propertyName](value);
-			} else {
-				this[propertyName].push(value);
-			}
-		},
-	});
+			if (!this.id){this.id = Math.random()}
+		}, {
+			validate: function(){return true},
+			save: function(){
+				if (this.validate()){Model.store.put(this)}
+			},
+			getIdentity: function(){
+				return Model.store.getIdentity(this);
+			},
+			get: function(propertyName){
+				if (this["get"+propertyName]){
+					//if a getter is defined
+					return this["get"+propertyName]();
+				} else {
+					return this[propertyName];
+				}
+			},
+			set: function(propertyName, value){
+				if (this["set"+propertyName]){
+					//if a setter is defined
+					this["set"+propertyName](value);
+				} else {
+					this[propertyName]=value;
+				}
+			},
+			add: function(propertyName, value){
+				if (this["add"+propertyName]){
+					//if a adder is defined
+					this["add"+propertyName](value);
+				} else {
+					this[propertyName].push(value);
+				}
+			},
+		}, {
+			query: function(query, options){
+				//TODO: return Model.store.query(lang.mixin(query, {_class: Tag}), options);
+				return Model.store.query(function(item){
+					return item instanceof this;
+				}.bind(this));
+			},
+			addRelationTo: function(targetModel, options){
+				var relationDefinition = lang.mixin({
+					sourceModel: this,
+					targetModel: targetModel
+				}, options);
+				Model.addRelation(relationDefinition);
+			},
+		}
+	);
 	Model.addRelation = function(relation){
 		//ajoute un getter sur la classe Model source
 		if (!relation.sourceModel.prototype["get"+relation.sourcePropertyName]){
 			relation.sourceModel.prototype["get"+relation.sourcePropertyName] = function(){	
 				var self = this;
 				return Model.store.query(function(item){
-					return item.isInstanceOf(relation.targetModel) && self[relation.sourcePropertyName] && self[relation.sourcePropertyName].indexOf(item.getIdentity())>= 0;
+					return item instanceof relation.targetModel && self[relation.sourcePropertyName] && self[relation.sourcePropertyName].indexOf(item.getIdentity())>= 0;
 				});
 			};
 		}
@@ -53,7 +68,7 @@
 			relation.targetModel.prototype["get"+relation.targetPropertyName] = function(){
 				var self = this;
 				return Model.store.query(function(item){
-					return item.isInstanceOf(relation.sourceModel) && item[relation.sourcePropertyName] && item[relation.sourcePropertyName].indexOf(self.getIdentity()) >= 0;
+					return item instanceof relation.sourceModel && item[relation.sourcePropertyName] && item[relation.sourcePropertyName].indexOf(self.getIdentity()) >= 0;
 				});
 			}
 		}
@@ -71,5 +86,6 @@
 			}
 		}
 	};
+	
 	return Model;
 });
--- model/tests/ModelRelations.js
+++ model/tests/ModelRelations.js
@@ -1,162 +1,183 @@
 ﻿define([
 	"doh/runner",
-	"SkFramework/model/Model",
-	"dojo/_base/declare",
+	"SkFramework/utils/identical",
+	"../Model",
+	"SkFramework/utils/create",
 	"dojo/store/Memory",
-], function(doh, Model, declare, Memory){
+	"SkFramework/store/Constructor",
+	"SkFramework/store/LocalStorage",
+], function(doh, identical, Model, create, Memory, Constructor, LocalStorage){
+
+	doh.i = doh.identical = function(expected, actual, sortArrays, hint){
+		if (! identical(expected, actual, sortArrays)){
+			throw new doh._AssertFailure("assertEqual() failed:\n\texpected\n\t\t"+JSON.stringify(expected)+"\n\tbut got\n\t\t"+JSON.stringify(actual)+"\n\n", hint);
+		}
+		return true;
+	};
 
 	//************* Models
-	Model.store = new Memory();
-	
-	window.Person = declare("Sk.Person", [Model], {
-		getage: function(){
-			return 2012 - this.get("birthYear");
-		},
-		getchildren: function(){
-			var union = this.get("motherOf");
-			union.push(this.get("fatherOf"));
-			return union;
-		},
-	});
-	
-	window.Todo = declare("Sk.Todo", [Model], {});
-	
-	window.Tag = declare("Sk.Tag", [Model], {});
-	
-	Model.addRelation({
-		sourceModel: Todo,
-		targetModel: Person,
-		sourcePropertyName: "user",
-		targetPropertyName: "todos",
-		min: 1,
-		max: 1,
-	});
-	
-	Model.addRelation({
-		sourceModel: Todo,
-		targetModel: Tag,
-		sourcePropertyName: "tags",
-		targetPropertyName: "todos",
-		min: 0,
-		max: null,
-	});
-	
-	Model.addRelation({
-		sourceModel: Person,
-		targetModel: Person,
-		sourcePropertyName: "conjoint",
-		targetPropertyName: "conjoint",
-		min: 0,
-		max: 1,
-	});
-	Model.addRelation({
-		sourceModel: Person,
-		targetModel: Person,
-		sourcePropertyName: "father",
-		targetPropertyName: "fatherOf",
-		min: 0,
-		max: 1,
-	});
-	Model.addRelation({
-		sourceModel: Person,
-		targetModel: Person,
-		sourcePropertyName: "mother",
-		targetPropertyName: "motherOf",
-		min: 0,
-		max: 1,
-	});
-	
-//*********** Instances
-	window.syv = new Person({
-		id: "1",
-		name: "Syv",
-		birthYear: 1982,
-	});
-	syv.save();
-
-	window.ket = new Person({name: "Ket"});
-	ket.save();
-	window.aur = new Person({name: "Aurélie"});
-	aur.save();
-	window.ant = new Person({name: "Antonin"});
-	ant.add("father", syv);
-	ant.add("mother", aur);
-	ant.save();
-	
-	aur.add("conjoint", syv);
-	aur.save();
-	
-	window.todo1 = new Todo({
-		id: "2",
-		title: "Concevoir la couche Model",
-	});
-	todo1.add("user", syv);
-	todo1.save();
-	
-	window.todo2 = new Todo({id: "3", title: "Concevoir la couche UI"});
-	todo2.add("user", syv);
-	todo2.save();
-	
-	window.todo3 = new Todo({title:"Lancer Maponaute"});
-	todo3.add("user", ket);
-	todo3.save();
-	
-	window.tag1 = new Tag({label: "test"});
-	tag1.save();
-	window.tag2 = new Tag({label: "cool"});
-	tag2.save();
-	window.tag3 = new Tag({label: "top"});
-	tag3.save();
-	
-	todo1.add("tags", tag1);
-	todo1.add("tags", tag2);
-	todo1.save();
-	tag1.add("todos", todo2);
-	todo2.save();
-
-//******* Tests
-	//function to compare that 2 arrays contain the same elements but possibly in a different order
-	function sameArray(a1, a2){
-		if (a1.length != a2.length){return false}
-		var delta = false;
-		a1.forEach(function(el){
-			if(a2.indexOf(el) == -1){delta=true}
+	function setUpModels(){		
+		window.Person = create(Model, function Person(){
+				this.superConstructor.apply(this, arguments);
+			}, {
+			getage: function(){
+				return 2012 - this.get("birthYear");
+			},
+			getchildren: function(){
+				var union = this.get("motherOf");
+				union.push(this.get("fatherOf"));
+				return union;
+			},
 		});
-		return !delta;
+		
+		window.Todo = create(Model, function Todo(){
+				this.superConstructor.apply(this, arguments);
+		});
+		
+		window.Tag = create(Model, function Tag(){
+				this.superConstructor.apply(this, arguments);
+		});
+		
+		Todo.addRelationTo(Person, {
+			sourcePropertyName: "user",
+			targetPropertyName: "todos",
+			min: 1,
+			max: 1,
+		});
+		
+		Todo.addRelationTo(Tag, {
+			sourcePropertyName: "tags",
+			targetPropertyName: "todos",
+			min: 0,
+			max: null,
+		});
+		
+		Model.addRelation({
+			sourceModel: Person,
+			targetModel: Person,
+			sourcePropertyName: "conjoint",
+			targetPropertyName: "conjoint",
+			min: 0,
+			max: 1,
+		});
+		Model.addRelation({
+			sourceModel: Person,
+			targetModel: Person,
+			sourcePropertyName: "father",
+			targetPropertyName: "fatherOf",
+			min: 0,
+			max: 1,
+		});
+		Model.addRelation({
+			sourceModel: Person,
+			targetModel: Person,
+			sourcePropertyName: "mother",
+			targetPropertyName: "motherOf",
+			min: 0,
+			max: 1,
+		});
 	};
 	
-	doh.register("Simple getter", function test0(t){
-		t.is(30, syv.get("age"), "syv should be 30");
-	});
-	doh.register("Relation resolution", {
+	//************* Instances
+	function setUpInstances(){		
+		window.syv = new Person({
+			id: "1",
+			name: "Syv",
+			birthYear: 1982,
+		});
+		syv.save();
+
+		window.ket = new Person({name: "Ket"});
+		ket.save();
+		window.aur = new Person({name: "Aurélie"});
+		aur.save();
+		window.ant = new Person({name: "Antonin"});
+		ant.add("father", syv);
+		ant.add("mother", aur);
+		ant.save();
+		
+		aur.add("conjoint", syv);
+		aur.save();
+		
+		window.todo1 = new Todo({
+			id: "2",
+			title: "Concevoir la couche Model",
+		});
+		todo1.add("user", syv);
+		todo1.save();
+		
+		window.todo2 = new Todo({id: "3", title: "Concevoir la couche UI"});
+		todo2.add("user", syv);
+		todo2.save();
+		
+		window.todo3 = new Todo({title:"Lancer Maponaute"});
+		todo3.add("user", ket);
+		todo3.save();
+		
+		window.tag1 = new Tag({label: "test"});
+		tag1.save();
+		window.tag2 = new Tag({label: "cool"});
+		tag2.save();
+		window.tag3 = new Tag({label: "top"});
+		tag3.save();
+		
+		todo1.add("tags", tag1);
+		todo1.add("tags", tag2);
+		todo1.save();
+		tag1.add("todos", todo2);
+		todo2.save();
+	};
+	
+//******* Tests
+	
+	testSet = {
+		"Simple getter": function(t){
+			t.is(30, syv.get("age"), "syv should be 30");
+		},
 		"user of todo1": function (t){
 			t.is(syv, todo1.get("user")[0], "The user of todo1 should be syv");
 		},
 		"syv's todos": function (t){
-			t.t(sameArray(syv.get("todos"), [todo2, todo1]));
+			t.i([todo2, todo1], syv.get("todos"), true);
 		},
 		"todo1 tags": function(t){
-			t.t(sameArray(todo1.get("tags"), [tag1, tag2]));
+			t.i([tag1, tag2], todo1.get("tags"), true);
 		},
 		"todos with test tag": function(t){
-			tag1.get("todos")
-		},
-	});
-	doh.register("Models query", {
-		"Person instances": function(t){
-			//query based on declaredClass name
-			t.t(sameArray(Model.store.query({declaredClass:"Sk.Person"}), [syv, aur, ket, ant]));
+			t.i([todo1, todo2], tag1.get("todos"), true);
 		},
 		"all Model instances": function(t){
-			//query based on isInstanceOf
-			t.t(sameArray(Model.store.query(function(item){return item.isInstanceOf(Model)}), [syv, aur, ket, ant, todo1, todo2, todo3, tag1, tag2, tag3]));
+			//t.i(Model.store.query(function(item){return item instanceof Model}), [syv, aur, ket, ant, todo1, todo2, todo3, tag1, tag2, tag3], true);
+			t.i([syv, aur, ket, ant, todo1, todo2, todo3, tag1, tag2, tag3], Model.query({}), true);
 		},
-	});
+		"Person instances": function(t){
+			t.i([syv, aur, ket, ant], Person.query({}), true);
+		},
 	/*
-	console.log("Les tags de la todo1 sont:", );
-	console.log("Les todos tagguées 'test' sont:", );
 	console.log("Le conjoint de syv est:", syv.get("conjoint"));
 	console.log("Le conjoint de Aurélie est:", aur.get("conjoint"));
 	console.log("Les enfants de Aurélie sont:", aur.get("children"));
 	*/
+		
+	};
+	
+	doh.register("Tests with Memory store", testSet, function setUp(){
+		Model.store = new Memory();
+		setUpModels();
+		setUpInstances();
+	});
+	
+	doh.register("Tests with LocalStorage store", testSet, function setUp(){
+		setUpModels();
+		Model.store = Constructor(new LocalStorage, {
+			constructorsMap: {
+				Person: Person,
+				Todo: Todo,
+				Tag: Tag,
+			}
+		});
+		Model.store.clear();
+		setUpInstances();
+	});
+	
 });
