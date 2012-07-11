df9019e01af1df2778f9f16920d4633c9a6e654e
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-07-11 13:19:48 +0200

    switchable store delegates to onLineStore

--- store/OnOffLineSwitcher.js
+++ store/OnOffLineSwitcher.js
@@ -1,11 +1,13 @@
 define(["dojo/_base/lang","dojo/_base/Deferred"
 ],function(lang, Deferred) {
 var Switcher = function(onLineStore, offLineStore, options){
-	var switcher = {};
-	switcher.onLine = function(){
-		//TODO: how to know if we are online ?
-		return false;
-	};
+	//why to delegate to onLineStore more than to offLineStore ? what could be better ?
+	var switcher = lang.delegate(onLineStore, {
+		onLine: function(){
+			return navigator.onLine;
+		},
+	});
+
 	var addMethod = function(method){
 		switcher[method] = function(){
 			if (this.onLine()){
@@ -16,7 +18,7 @@ var Switcher = function(onLineStore, offLineStore, options){
 		};
 	};
 
-	["get", "put", "remove", "add"].forEach(addMethod);
+	["get", "put", "remove", "add", "query"].forEach(addMethod);
 
 	lang.mixin(switcher, options);
 
