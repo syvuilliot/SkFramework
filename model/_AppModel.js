c8964f2746a807c5376844b6e709b403a5333dca
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-08-21 18:48:40 +0200

    AppModel from SkTodos

---
+++ model/_AppModel.js
@@ -0,0 +1,15 @@
+define([
+	'dojo/_base/declare',
+	'dojo/Stateful',	'dojo/Evented'
+],
+function(
+	declare,
+	Stateful,			Evented
+) {
+	return declare([Stateful, Evented], {
+		constructor: function(params) {
+			declare.safeMixin(this, params);
+		},
+
+	});
+});
