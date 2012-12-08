54bd8e7f828a4de9b47ffb4d6f9341b72771add2
Sylvain Vuilliot <syvuilliot@gmail.com>
2012-12-08 11:13:08 +0100

    Select form component

--- components/form/Select.js
+++ components/form/Select.js
@@ -1,21 +1,43 @@
 define([
 	"dojo/_base/declare",
-	"SkFramework/components/Repeater",
+	"SkFramework/components/repeater/Repeater",
+	"SkFramework/component/DomComponent",
+	'SkFramework/component/Presenter',
 	"frb/bind",
 	"put-selector/put",
 	"SkFramework/utils/frb-dom",
-], function(declare, Repeater, bind, put){
+], function(declare, Repeater, DomComponent, PresenterBase, bind, put){
+
+	var Option = declare(DomComponent, {
+		domTag: "option",
+		constructor: function(params){
+			this._cancelLabelBinding = bind(this, "domNode.label", {"<-": "_presenter.value."+params.labelProp});
+		},
+		destroy: function(){
+			this._cancelLabelBinding();
+			this.inherited(arguments);
+		},
+	});
 
 	return declare(Repeater, {
 		domTag: "select",
 		collectionProperty: "options",
-		constructor: function(){
-			this._cancelValueBinding = bind(this, "domNode.value", {"<->": "_presenter.value"});
-		},
-		createComponent: function(option){
-			return put("option", {
-				value: option.value,
-				innerHTML: option.label
+		componentConstructor: Option,
+		constructor: function(params){
+			this.componentConstructorArguments = {
+				labelProp: params.labelProp || "label",
+			};
+			this._cancelValueBinding = bind(this, "domNode.selectedIndex", {
+				"<->": "_presenter.value",
+				convert: function(value){
+					var options = this._presenter.options;
+					return options ? options.indexOf(value) : -1;
+				},
+				revert: function(selectedIndex){
+					var options = this._presenter.options;
+					return options ? options[selectedIndex] : undefined;
+				},
+				trace: true,
 			});
 		},
 		destroy: function(){
