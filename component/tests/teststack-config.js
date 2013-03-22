a521f18d1024992d7e180df47ec9f2a0f53ae909
ks <ks@ordiket.(none)>
2013-03-22 09:23:10 +0100

    Test runner config

--- component/tests/teststack-config.js
+++ component/tests/teststack-config.js
@@ -3,15 +3,12 @@ define({
 	// used here
 	loader: {
 		// Packages that should be registered with the loader in each testing environment
-		packages: [ 'dojo2-teststack' ],
-		map: { 'dojo2-teststack': { 'dojo-ts': 'dojo2-teststack/dojo' } },
-		paths: {
-			"collections": "collections-amd",
-			"frb": "frb-amd",
-		},
 	},
 
 	// Non-functional test suite(s) to run in each browser
-	suites: [ 'SkFramework/component/tests/Component' ],
+	suites: [
+		'SkFramework/component/tests/Component',
+		'SkFramework/component/tests/DomComponent'
+	],
 
 });
