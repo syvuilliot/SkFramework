4e2eba18e9023b007fcf643ccf9ba6e680dae358
ks <ks@ordiket.(none)>
2013-03-22 13:56:14 +0100

    Reorganized tests launching

--- tests/teststack-config.js
+++ tests/teststack-config.js
@@ -2,16 +2,19 @@ define({
 	// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
 	// used here
 	loader: {
-		// Packages that should be registered with the loader in each testing environment
-		packages: [ 'dojo2-teststack' ],
-		map: { 'dojo2-teststack': { 'dojo-ts': 'dojo2-teststack/dojo' } },
+		packages: [
+			{ name: 'chai', location: 'dojo2-teststack/chai', main: 'chai' }
+		],
 		paths: {
 			"collections": "collections-amd",
 			"frb": "frb-amd",
-		},
+		}
 	},
 
 	// Non-functional test suite(s) to run in each browser
-	suites: [ 'SkFramework/utils/tests/Registry' ],
+	suites: [
+		'ksf/component/tests/Component',
+		'ksf/component/tests/DomComponent'
+	],
 
 });
