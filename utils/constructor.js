0ae1094944fbfb766b07c6ec244f2f4b44a88925
Sylvain Vuilliot <syvuilliot@gmail.com>
2013-04-02 17:39:49 +0200

    allow multi signatures for constructor

--- utils/constructor.js
+++ utils/constructor.js
@@ -3,31 +3,34 @@ define([
 ], function(
 	mixin
 ) {
-	var constructor = function(base, init, proto) {
-		var Constructor;
-		switch (arguments.length){
-			case 3:
-				Constructor = function(){
-					base.apply(this, arguments);
-					init.apply(this, arguments);
-				};
-				Constructor.prototype = Object.create(base.prototype);
-				mixin(Constructor.prototype, init.prototype, proto);
-				// Constructor.displayName = "toto";
+	var constructor = function(base, Constructor, proto) {
+		switch (typeof base){
+			case "function":
+				switch (typeof Constructor){
+					case "function":
+						var originalPrototype = Constructor.prototype;
+						Constructor.prototype = Object.create(base.prototype);
+						mixin(Constructor.prototype, originalPrototype);
+						break;
+					case "object":
+						proto = Constructor;
+						Constructor = base;
+						break;
+					default:
+						Constructor = base;
+				}
 				break;
-			case 2:
-				Constructor = base;
-				proto = init;
-				mixin(Constructor.prototype, proto);
-				break;
-			case 1:
+			case "object": // only a prototype is provided
 				Constructor = function(){};
 				proto = base;
-				mixin(Constructor.prototype, proto);
+				break;
+			default:
+				throw "Invalid arguments";
 		}
+		proto && mixin(Constructor.prototype, proto);
 		// optional
-		Constructor.extends = function(init, proto){
-			return constructor(Constructor, init, proto);
+		Constructor.extends = function(ctr, proto){
+			return constructor(Constructor, ctr, proto);
 		};
 		return Constructor;
 	};
