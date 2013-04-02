9c06e8b25c6e07c1d91f23d18a7314017cd470b6
Sylvain Vuilliot <syvuilliot@gmail.com>
2013-04-02 11:26:23 +0200

    allow inheritance with constructor creator

--- utils/constructor.js
+++ utils/constructor.js
@@ -4,13 +4,28 @@ define([
 	mixin
 ) {
 	var constructor = function(base, init, proto) {
-		var Constructor = function(){
-			base.apply(this, arguments);
-			init.apply(this, arguments);
-		};
-		Constructor.prototype = Object.create(base.prototype);
-		mixin(Constructor.prototype, init.prototype, proto);
-		// Constructor.displayName = "toto";
+		var Constructor;
+		switch (arguments.length){
+			case 3:
+				Constructor = function(){
+					base.apply(this, arguments);
+					init.apply(this, arguments);
+				};
+				Constructor.prototype = Object.create(base.prototype);
+				mixin(Constructor.prototype, init.prototype, proto);
+				// Constructor.displayName = "toto";
+				break;
+			case 2:
+				Constructor = base;
+				proto = init;
+				mixin(Constructor.prototype, proto);
+				break;
+			case 1:
+				Constructor = function(){};
+				proto = base;
+				mixin(Constructor.prototype, proto);
+		}
+		// optional
 		Constructor.extends = function(init, proto){
 			return constructor(Constructor, init, proto);
 		};
