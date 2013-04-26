262ad6ce88aab1b1016e6e6bc2f4622a86102c6b
Sylvain Vuilliot <syvuilliot@gmail.com>
2013-04-26 09:54:25 +0200

    ActiveGrid early release

---
+++ components/tests/ActiveGrid.js
@@ -0,0 +1,90 @@
+define([
+	'teststack!object',	'teststack/chai!assert',
+	"../ActiveGrid",
+
+	"frb/bind",
+	"frb/observe",
+	"ksf/utils/frb-dom",
+], function(
+	registerSuite, assert,
+	Grid,
+	bind,
+	observe,
+	SortedArray
+){
+	// create css rules
+	var css = document.createElement("style");
+	css.type = "text/css";
+	document.head.appendChild(css);
+	css.sheet.insertRule('.active { background-color: red; }', css.sheet.cssRules.length);
+	css.sheet.insertRule('.selected { background-color: blue; }', css.sheet.cssRules.length);
+
+
+	var syv = window.syv = {name: "Sylvain", age: 31, sexe: "M"};
+	var aur = window.aur = {name: "Aurélie", age: 30, sexe:"F"};
+	var ant = window.ant = {name: "Antonin", age: 2, sexe:"M"};
+	var leo = window.leo = {name: "Léonie", age: 1, sexe:"F"};
+	var collection = window.collection = [syv, aur, ant];
+
+	var grid = window.grid = new Grid({});
+	document.body.appendChild(grid.domNode);
+
+	grid.value = collection;
+
+	var InputRenderer = function(prop){
+		return {
+			create: function(item){
+				var cmp = document.createElement("input");
+				cmp.destroy = bind(cmp, "value", {
+					"<->": prop,
+					source: item,
+				});
+				return cmp;
+			},
+			destroy: function(item, cmp){
+				cmp.destroy();
+			},
+		};
+	};
+
+	var columns = window.columns = [{
+		header: "Nom",
+		body: {
+			factory : InputRenderer("name"),
+		}
+	}, {
+		header: "Age",
+		body: {
+			factory : InputRenderer("age"),
+		}
+	}];
+
+	grid.columns = columns;
+
+	// add column
+	columns.push({header: "Sexe", body: {
+		factory: InputRenderer("sexe"),
+	}});
+
+	// add row
+	collection.push(leo);
+
+	// sort collection
+	// collection.reverse();
+
+	// sort columns
+	// config.reverse();
+
+
+	// observe activeItem
+	observe(grid, "activeItem", function(item){
+		console.log("active item changed to", item.name);
+	});
+
+	// select via a click
+
+	// select programatically
+	grid.activeItem = leo;
+
+
+});
