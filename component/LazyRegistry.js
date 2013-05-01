1d33e39fca9ed7a86016962cbc139acb0726f294
Sylvain Vuilliot <syvuilliot@gmail.com>
2013-05-01 14:41:46 +0200

    lazyRegistry

--- component/LazyRegistry.js
+++ component/LazyRegistry.js
@@ -1,25 +1,25 @@
 define([
 	"ksf/utils/constructor",
 	"collections/map",
-	"ksf/component/Registry",
+	"ksf/utils/IndexedSet",
 ], function(
 	ctr,
 	Map,
-	Registry
+	IndexedSet
 ){
 	/**
 	Registry of components that are created on demand on "get" and are destroyed (forgotten) if nobody use them
 	*/
-	return ctr(Registry, function(factory){
-		Registry.apply(this, arguments);
+	return ctr(function(args){
+		this._registry = args.registry || new IndexedSet();
 		this._usersCount = new Map();
-		this._factory = factory;
+		this._factory = args.factory;
 	}, {
 		get: function(id){
-			var cmp = this._components.getValues(id)[0];
+			var cmp = this._registry.get(id);
 			if (!cmp){
 				cmp = this._factory.create(id);
-				this.add(cmp, id);
+				this._registry.add(cmp, id);
 			}
 			if (cmp){
 				this._usersCount.set(cmp, (this._usersCount.get(cmp) || 0)+1);
@@ -27,12 +27,12 @@ define([
 			return cmp;
 		},
 		release: function(id){
-			var cmp = this._components.getValues(id)[0];
+			var cmp = this._registry.get(id);
 			var count = (this._usersCount.get(cmp) || 0)-1;
 			if (count <=0){
 				this._usersCount.delete(cmp);
 				this._factory.destroy(id, cmp);
-				this.delete(cmp);
+				this._registry.remove(cmp);
 			} else {
 				this._usersCount.set(cmp, count);
 			}
