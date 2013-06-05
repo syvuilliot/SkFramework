f1a4cb0841fba504c9821bb6790433420da215ea
Sylvain Vuilliot <syvuilliot@gmail.com>
2013-06-05 13:56:44 +0200

    syncIdProperty parameter in WithRelationSerialize

--- model/propertyManagers/WithRelationSerialize.js
+++ model/propertyManagers/WithRelationSerialize.js
@@ -4,14 +4,17 @@ define([
 
 ){
 	return function(args){
+		var syncIdProperty = args.syncIdProperty || "id";
 		this.serialize = function(rsc){
-			return args.manager.getPropValue(rsc, "syncId");
+			return args.manager.getPropValue(rsc, syncIdProperty);
 		};
 		this.deserialize = function(id){
 			// ce n'est pas au resource manager d'être lazy, car le cas dans lequel on souhaite être lazy, c'est celui de la résolution d'id, donc on le fait ici
-			var rsc = args.manager.getBy("syncId", id) || args.manager.create({
-				syncId: id,
-			});
+			var rsc = args.manager.getBy(syncIdProperty, id);
+			if (!rsc){
+				rsc = args.manager.create();
+				args.manager.setPropValue(rsc, syncIdProperty, id);
+			}
 			return rsc;
 		};
 		this.serializePropName = args.serializePropName;
