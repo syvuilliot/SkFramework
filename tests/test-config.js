5cea2a497b2203befcaa00d01f8351f5d19bf935
Quentin <quentin.vuilliot@kaes.fr>
2013-07-26 19:14:27 +0200

    Use of a common config for require across tests/demos

--- tests/test-config.js
+++ tests/test-config.js
@@ -1,21 +1,13 @@
-define({
-	// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
-	// used here
-	loader: {
-		packages: [
-			{ name: 'compose', location: 'compose', main: 'compose' },
-			{ name: 'bacon.js', location: 'bacon.js/dist', main: 'Bacon' }
-		],
-		paths: {
-			"collections": "collections-amd",
-			"frb": "frb-amd"
-		}
-	},
+define(['./require-config'], function(loaderConfig) {
+	return {
+		// Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
+		// used here
+		loader: loaderConfig,
 
-	// Non-functional test suite(s) to run in each browser
-	suites: [
-		'ksf/component/tests/all',
-		'ksf/utils/tests/all'
-	],
-
+		// Non-functional test suite(s) to run in each browser
+		suites: [
+			'ksf/component/tests/all',
+			'ksf/utils/tests/all'
+		]
+	};
 });
